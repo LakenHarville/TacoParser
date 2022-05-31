cb1759894378f16b1f492764e7fa87bfd1282481
Laken Harville <rlakenharville@gmail.com>
2022-05-31 17:14:06 -0500

    Initial commit

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -27,17 +27,30 @@ namespace LoggingKata.Test
         {
             // TODO: Complete - "line" represents input data we will Parse to
             //       extract the Longitude.  Your .csv file will have many of these lines,
-            //       each representing a TacoBell location
+            //       each representing a TacoBell location - Done
 
             //Arrange
+            var tester = new TacoParser();
 
             //Act
+            var actual = tester.Parse(line);
 
             //Assert
+            Assert.Equal(expected, actual.Location.Longitude);
         }
 
 
-        //TODO: Create a test ShouldParseLatitude
+        //TODO: Create a test ShouldParseLatitude - Done
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        public void ShouldParseLatitude(string line, double expected)
+        {
+            var tester = new TacoParser();
+            var actual = tester.Parse(line);
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
+
+
 
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -19,26 +19,37 @@
             {
                 // Log that and return null
                 // Do not fail if one record parsing fails, return null
+                logger.LogWarning("Incomplete data.");
                 return null; // TODO Implement
             }
 
-            // grab the latitude from your array at index 0
-            // grab the longitude from your array at index 1
-            // grab the name from your array at index 2
+            // grab the latitude from your array at index 0 - Done
+            var latitude = double.Parse(cells[0]);
+            // grab the longitude from your array at index 1 - Done
+            var longitude = double.Parse(cells[1]);
+            // grab the name from your array at index 2 - Done
+            var name = cells[2];
 
-            // Your going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
+            // Your going to need to parse your string as a `double` - Done
+            // which is similar to parsing a string as an `int` - Done
 
-            // You'll need to create a TacoBell class
-            // that conforms to ITrackable
+            // You'll need to create a TacoBell class - Done 
+            // that conforms to ITrackable - Done
 
-            // Then, you'll need an instance of the TacoBell class
-            // With the name and point set correctly
+            // Then, you'll need an instance of the TacoBell class - Done
+            // With the name and point set correctly - Done
+            var point = new Point();
+            point.Longitude = longitude;
+            point.Latitude = latitude;
 
+
+            var tacoBell = new TacoBell();
+            tacoBell.Name = name;
+            tacoBell.Location = point;
             // Then, return the instance of your TacoBell class
             // Since it conforms to ITrackable
 
-            return null;
+            return tacoBell;
         }
     }
 }
